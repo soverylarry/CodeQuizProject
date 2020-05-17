db0908d69b06ab8f225d8b2b3482c7c0869e8c9f
larry laconi <soverylarry@gmail.com>
2020-05-16 23:01:09 -0400

    pushing uncompleted project Code

---
+++ question.js
@@ -0,0 +1,80 @@
+var questions = [{
+    "question": "what is Javasript", 
+    "option1": "scripting language",
+    "option2": "boolean value",
+    "option3": "ancient scroll",
+    "option4": "stuff",
+    "answer": "1"
+},
+
+{
+    question: "what year was Javascript started?",
+    
+    "option1": "1903",
+    "option2": "2001",
+    "option3": "2020",
+    "option4": "1995",
+    "answer": "4"
+},
+
+{
+    question: "what is an example of a Boolean?",
+    
+    "option1": "1903",
+    "option2": "yadda yadda",
+    "option3": "true/false",
+    "option4": "your Mom",
+    "answer": "3"
+},
+
+{
+    question: "inside which HTML element do you put Javascript?",
+    
+    "option1": "snow",
+    "option2": "script",
+    "option3": "uranium",
+    "option4": "tupperware",
+    "answer": "2"
+},
+
+{
+    question: "where do you insert Javascript?",
+  
+    "option1": "head",
+    "option2": "body",
+    "option3": "head or body",
+    "option4": "Uranus",
+    "answer": "3"
+},
+
+{
+    question: "at which company was JS first developed?",
+   
+    "option1": "IBM",
+    "option2": "Netscape",
+    "option3": "SkyNet",
+    "option4": "Friendster",
+    "answer": "2"
+},
+
+{
+    question: "who created Javascript?",
+ 
+    "option1": "CarrotTop",
+    "option2": "Doc Brown",
+    "option3": "Steve Ballmer",
+    "option4": "Brendan Eich",
+    "answer": "4"
+},
+
+{
+    question: "what is ECMAscript?",
+    
+    "option1": "doctors perscription",
+    "option2": "early sanskrit",
+    "option3": "standardized JS",
+    "option4": "an ugly itch",
+    "answer": "3"
+},
+
+]
---
+++ script.js
@@ -0,0 +1,61 @@
+//declaring key variables
+var currentQuestion = 0
+//var timeLeft = 120 (to be used when I figure out the damn timer...)
+var score = 0;
+var totQuestions = questions.length;
+
+var container = document.getElementById('quizContainer');
+var questionEl = document.getElementById('question');
+var opt1 = document.getElementById('opt1');
+var opt2 = document.getElementById('opt2');
+var opt3 = document.getElementById('opt3');
+var opt4 = document.getElementById('opt4');
+var nextButton = document.getElementById('nextButton');
+var resultCont = document.getElementById('result');
+
+//method to load questions from question index
+function loadQuestion (questionIndex) {
+    var q = questions[questionIndex];
+    questionEl.textContent = (questionIndex + 1) + '. ' + q.question;
+    opt1.textContent = q.option1;
+    opt2.textContent = q.option2;
+    opt3.textContent = q.option3;
+    opt4.textContent = q.option4;
+};
+//loads next question after checking to see an answer has been selected
+function loadNextQuestion () {
+    var selectedOption = document.querySelector('input[type=radio]:checked');
+    if(!selectedOption){
+        alert('please make a choice!');
+        return;
+    }
+    //awards 10 points correct answer after verifying correct answer
+    var answer = selectedOption.value;
+    if(questions[currentQuestion].answer == answer){
+        score += 10;
+    }
+    //if last question shows that you're done
+    selectedOption.checked = false;
+    currentQuestion++;
+    if(currentQuestion == totQuestions - 1){
+        nextButton.textContent = 'Finish';
+    }
+    //at end of quiz the container will disappear and shows results container with score
+    if(currentQuestion == totQuestions){
+        container.style.display = 'none';
+        resultCont.style.display = '';
+        resultCont.textContent = 'Your Score: ' + score;
+        return;
+    }
+    loadQuestion(currentQuestion);
+}
+//trying to jump start quiz but couldn't get questions to load....'questions' showing as undefined
+loadQuestion(currentQuestion)
+
+
+
+//save Initials and Score to local storage
+
+
+        
+
