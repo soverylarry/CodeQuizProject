0f41f1f1c9b04483716a83a56ada213883f5ea5b
larry laconi <soverylarry@gmail.com>
2020-05-18 16:33:53 -0400

    adding timer function

--- saveLocalScore.js
+++ saveLocalScore.js
@@ -1,31 +1,15 @@
 //save Initials and Score to local storage...not working yet...dammmit....
 
-var count = 30;
-var counter = setInterval(timer, 1000);
+var score = 0
+var highscore = localStorage.getItem("highscore");
 
-$(document).ready(function() {
-    if (typeof localStorage["highScore"] !== 'undefined') {
-    $(".score").text('Your current high score is' + " " + localStorage["highScore"]);}
-    else {
-      localStorage["highScore"] = 0;
-      $(".score").text('Your current high score is 0');
+
+
+if(highscore !== null){
+    if (score > highscore) {
+        localStorage.setItem("highscore", score);      
     }
-    let counter = {
-      sec: 0,
-      count: 0,
-      inc() {
-        this.count++;
-        $(".counter").text(this.count);
-      },
-      score: 0
-    }
-  
-    function store() {
-      if (counter.score > localStorage["highScore"]) {
-        localStorage["highScore"] = counter.score;
-      }
-  
-      if (typeof localStorage["highScore"] !== 'undefined') {
-    $(".score").text('Your current high score is' + " " + localStorage["highScore"]);}
-    }
-})
+}
+else{
+    localStorage.setItem("highscore", score);
+}
